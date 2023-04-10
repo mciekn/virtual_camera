c0875b1e76a6b14f7a4e465d403db40d709962ba
Maciej Niszczota <macni.macni@gmail.com>
2023-04-10 14:18:27 +0200

    Init commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module pl.gk.virtual_camera {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens pl.gk.virtual_camera to javafx.fxml;
+    exports pl.gk.virtual_camera;
+}
